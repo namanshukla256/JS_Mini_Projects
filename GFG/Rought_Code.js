427a1e787819cd4ad4ceeaa4abf6fa474e4e5174
naman <namanshukla256@gmail.com>
2025-01-22 10:14:21 +0530

    22/1

--- GFG/Rought_Code.js
+++ GFG/Rought_Code.js
@@ -1,8 +1,37 @@
-// Creating an Object
-const person = {};
 
-// Adding Properties
-person.firstName = "Naman";
-person.lastName = "Shukls";
-person.age = 25;
-person.eyeColor = "blue";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const person = new Object();
+
+// person.firstName = "Naman";
+// person.lastName = "Shukls";
+// person.age = 25;
+// person.eyeColor = "blue";
+
+
+
+
+
+// // Creating an Object
+// const person = {};
+
+// // Adding Properties
+// person.firstName = "Naman";
+// person.lastName = "Shukls";
+// person.age = 25;
+// person.eyeColor = "blue";
