80b69e0f92dc52fb0229705949d99cdf655e188e
[Naman Shukla] <[namanshukla256@gmail.com]>
2025-01-30 23:59:01 +0530

    30-1-2025

---
+++ GFG/Price Range Slider with Min-Max Input/script.js
@@ -0,0 +1,5 @@
+const rangevalue = document.querySelectorAll(".slider-container .price-slider");
+const rangeInputvalue = document.querySelectorAll(".range-input input");
+
+// Setup the price gap
+
